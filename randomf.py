14993fca64555933d7808984f26be47f6608fec8
97YearsOldProgrammer <dsbchen@ucdavis.edu>
2025-06-13 17:21:14 -0700

    up start hhh

--- randomf.py
+++ randomf.py
@@ -1,37 +1,30 @@
-def next_one(array, pos, len):
-    ''' find nearest element in an array '''
-    
-    for i, num in enumerate(array):
-        if  num <= pos: continue
-        if  pos - num < len: continue
-        del array[:i]   
-        return num
-    
-    return None
-          
-def fw_rnatree(dons, accs, min_intron, min_exon):
-    ''' forward RNA tree for random forest '''
+import random
 
-    isoform = []
-    def tree(pos):
-        ''' greedy decision tree '''
-        
-        doner = next_one(dons, pos, min_exon   + 1)
-        accer = next_one(accs, pos, min_intron - 1)
-        
-        if doner == None or accer == None: return
-        
-        if  doner > accer: 
-            isoform.append(doner)
-            pos = doner
-        else:
-            isoform.append(accer)
-            pos = accer
-        
-        tree(pos)
+############################################################
+############################################################
+##################### DecisionTree Class ###################
+############################################################
+############################################################
+
+class IsoformTree:
+    def __init__(self, dons, accs, dict, mtry, max_depth=None):
+        self.dons = dons
+        self.accs = accs
+        self.dict = dict
+        self.mtry = mtry
+        self.max_depth = max_depth
     
-    tree(min_exon)
-    return isoform
+    def btstrapping(dons, accs):
+        ''' boostrapping original donor and acceptor sites '''
+    
+def btstrapping(dons, accs, size=None):
+    ''' boostrapping original donor and acceptor sites '''
+    
+    if size is None:    size = len(dons)
+    dons = sorted(random.choices(dons, k=size))
+    
+    if size is None:    size = len(accs)
+    accs = sorted(random.choices(accs, k=size))
+    
+    return dons, accs
         
-def bw_rnatree(dons, accs, min_intron, min_exon, pexon, pintron):
-    pass
