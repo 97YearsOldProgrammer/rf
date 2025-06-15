be01d9eb9218d6d6d8eb90d7938cbbb712fb45d1
97YearsOldProgrammer <dsbchen@ucdavis.edu>
2025-06-15 15:48:39 -0700

    up

--- randomf.py
+++ randomf.py
@@ -1,9 +1,33 @@
 import random
 import numpy as np
 
-def finding_outlier(dons, accs, dict):
-    if dons
-    
+def finding_outlier(doa, pos2info, z_threshold=None):
+    '''
+    doa     :   dons or accs
+    pos2info:   dictionary for pos : val, typ
+    '''
+    assert(doa)
+
+    mdoa = []
+
+    for pos in doa:
+        val, typ = pos2info[pos]
+        mdoa.append((pos, typ, val))
+
+    mdoa = sorted(mdoa, key=lambda x: x[2])
+    vals  = np.asarray([val for _, _, val in mdoa], dtype=float)
+    mean  = vals.mean()
+    std   = vals.std(ddof=0)
+    zscr  = (vals - mean)/std
+
+    if not z_threshold: otest = np.abs(zscr) >= 3.0
+    else:               otest = np.abs(zscr) >= z_threshold
+
+    otl   = [info for info, tf in zip(mdoa, otest) if tf]
+    doa   = [info for info, tf in zip(mdoa, otest) if not tf]
+
+    return otl, doa
+
 ############################################################
 ############################################################
 ##################### DecisionTree Class ###################
@@ -26,27 +50,29 @@ class IsoformTree:
         
     def _btstrapping(self):
         ''' boostrapping original donor and acceptor sites '''
+
         self.dons = sorted(random.choices(self.dons, k=len(self.dons) ))
         self.accs = sorted(random.choices(self.accs, k=len(self.accs) ))
         
     def f_subset(self, node):
         ''' find random subset of current dataset '''
-        array = np.random.choice(node, size=self.mtry, replace=False)
-        return array
+
+        subset = np.random.choice(node, size=self.mtry, replace=False)
+        return subset
     
     def compute_mse(self, array):
         ''' compute the mean squared error '''
+
         arr  = np.asarray(array, dtype=float)
         return np.mean( (arr - arr.mean())**2 ) if arr.size else 0.0
     
-    def mse_split(self, node):
+    def split_mse(self, node):
         ''' compute the mean squared error gain for split criteria '''
-        subset     = 0 # back later on, not quite sure yet
+
+        subset     = self.f_subset(node)
         parent_mse = self.compute_mse(subset)
         
-        
         all_psplit = dict()
-        all_gain   = []
         
         for i in range(len(subset)):
             gain_mse   = parent_mse
